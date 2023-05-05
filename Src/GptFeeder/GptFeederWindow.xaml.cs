23f28b76cd153f46be7f950873b0fa5f95ab259f
alexpisqueared <alex.pigida@outlook.com>
2023-05-05 09:39:59 -0400

    bettered

--- Src/GptFeeder/GptFeederWindow.xaml.cs
+++ Src/GptFeeder/GptFeederWindow.xaml.cs
@@ -6,43 +6,37 @@ public partial class GptFeederWindow : Window
 {
   public GptFeederWindow() => InitializeComponent();
 
-  int current = 0, ttl = 0, l = 0;
+  int current = 0, ttl = 0, curPos = 0;
+  const int max = 2000;
 
   void OnSplit(object sender, RoutedEventArgs e) { }
   void OnClose(object sender, RoutedEventArgs e) => Close();
 
-  private void OnZoom(object sender, System.Windows.Input.MouseWheelEventArgs e) => ((TextBox)sender).FontSize += e.Delta > 0 ? 1 : -1;
+  void OnZoom(object sender, System.Windows.Input.MouseWheelEventArgs e) => ((TextBox)sender).FontSize += e.Delta > 0 ? 1 : -1;
 
   void OnNext(object sender, RoutedEventArgs e)
   {
-    if (current++ == 0)
-    {
-      var s = $"Concatenate this and next {ttl} messages: ";
-      l = 2000 - s.Length;
-      Stp.Text = s + Src.Text[..l];
-    }
+    var ini = (current++ == 0) ? $"Concatenate this and next {ttl} messages: " : $"Message {current}: ";
+    var len = max - ini.Length;
+    if (curPos + len < Src.Text.Length)
+      Stp.Text = $"{ini}{Src.Text.Substring(curPos, len)}";
     else
     {
-      var s = $"Message {current}: ";
-      var l2 = 2000 - s.Length;
-      if (l + l2 < Src.Text.Length)
-        Stp.Text = s + Src.Text.Substring(l, l2);
-      else
-      {
-        Stp.Text = s + Src.Text[l..]; current = l = 0; 
-        System.Media.SystemSounds.Asterisk.Play();
-      }
-
-      l += l2;
+      Stp.Text = $"{ini}{Src.Text[curPos..]}"; 
+      current = curPos = 0;
+      System.Media.SystemSounds.Asterisk.Play();
     }
 
+    curPos += len;
+
     Clipboard.SetText(Stp.Text);
-    Rpt.Text = $"{current} / {ttl}   {l} / {Src.Text.Length}";
+
+    Rpt.Text = $"{current} / {ttl}   {curPos} / {Src.Text.Length}";
   }
   void OnTextChanged(object sender, TextChangedEventArgs e)
   {
     if (Rpt is null) return; Rpt.Text = $"{Src.Text.Length} chars ";
 
-    ttl = (Src.Text.Length + 100) / 2000;
+    ttl = (Src.Text.Length + 100) / max;
   }
 }
